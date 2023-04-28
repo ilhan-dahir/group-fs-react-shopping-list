597be1e50f36cfde67aa4c9484b907191d9b86be
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-28 09:52:56 -0500

    Built confirmation box for reset
    - With sweetalert2 built out a confirmation pop-up for the reset button within
       ItemList.jsx

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,17 +1,43 @@
 import axios from 'axios';
 import Item from './Item/Item';
-import './ItemList.css'
+import './ItemList.css';
+import Swal from 'sweetalert2';
 
 function ItemList(props) {
     function handleReset() {
-        axios({
-            method: 'PUT',
-            url: '/items/'
-        }).then((response) => {
-            props.getItems();
-        }).catch((error) => {
-            console.log('The Reset failed miserably');
+        Swal.fire({
+            icon: 'warning',
+            title: 'Reset Confirmation',
+            text: 'Are you sure you want to reset the purchase status of All items?',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, I want to reset.',
+            cancelButtonText: 'No, I want to cancel!',
+            // reverseButtons: true
+        }).then((result) => {
+            if(result.isConfirmed) {
+                axios({
+                    method: 'PUT',
+                    url: '/items/'
+                }).then((response) => {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Reset Complete',
+                        text: 'Your list has been reset!'
+                    })
+                    props.getItems();
+                }).catch((error) => {
+                    console.log('The Reset failed miserably');
+                })
+            }
+            else if (result.dismiss === Swal.DismissReason.cancel){
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Phew!',
+                    text: 'That was close, your list has NOT been reset'
+                })
+            }
         })
+
     }
     function handleDelete() {
         axios({
