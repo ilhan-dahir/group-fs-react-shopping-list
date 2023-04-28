d7c6e21fd8f0d8fc295091d662c549fa9447bd8b
Joshua Engebretson <joshua.engebretson@gmail.com>
2023-04-28 10:04:44 -0500

    Created confirmation for clear button
    - Made with sweetalert2, confirmation now exists for clear button within
       ItemList.jsx

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -5,6 +5,7 @@ import Swal from 'sweetalert2';
 
 function ItemList(props) {
     function handleReset() {
+        // Confirmation box before Resetting
         Swal.fire({
             icon: 'warning',
             title: 'Reset Confirmation',
@@ -12,24 +13,33 @@ function ItemList(props) {
             showCancelButton: true,
             confirmButtonText: 'Yes, I want to reset.',
             cancelButtonText: 'No, I want to cancel!',
-            // reverseButtons: true
+            reverseButtons: true
         }).then((result) => {
             if(result.isConfirmed) {
+                //If confirmed, complete the reset with PUT request
                 axios({
                     method: 'PUT',
                     url: '/items/'
                 }).then((response) => {
+                    // If axios request succesful, alert the user
                     Swal.fire({
                         icon: 'success',
                         title: 'Reset Complete',
                         text: 'Your list has been reset!'
                     })
+                    // Then rerun getItems
                     props.getItems();
                 }).catch((error) => {
+                    // If axios request failed, alert the user
+                    Swal.fire({
+                        icon: 'warning',
+                        text: 'An error occured with the reset, please try again later.'
+                    })
                     console.log('The Reset failed miserably');
                 })
             }
             else if (result.dismiss === Swal.DismissReason.cancel){
+                // If cancelled, alert the user that their list was not reset
                 Swal.fire({
                     icon: 'error',
                     title: 'Phew!',
@@ -37,17 +47,49 @@ function ItemList(props) {
                 })
             }
         })
-
     }
+
     function handleDelete() {
-        axios({
-            method: 'Delete',
-            url: '/items/'
-        }).then((response) => {
-            console.log('All items are deleted');
-            props.getItems();
-        }).catch((error) => {
-            console.log("Delete failed ");
+        Swal.fire({
+            icon: 'warning',
+            title: 'Delete Confirmation',
+            text: 'Are you sure you want to remove all the items from your list?',
+            showCancelButton: true,
+            confirmButtonText: 'Yes, I want to erase everything and start over.',
+            cancelButtonText: 'No, I want to cancel!',
+            reverseButtons: true
+        }).then((result) => {
+            if(result.isConfirmed) {
+                //If confirmed, complete the reset with PUT request
+                axios({
+                    method: 'Delete',
+                    url: '/items/'
+                }).then((response) => {
+                    // If axios request succesful, alert the user
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Clear Complete',
+                        text: 'All items have been deleted!'
+                    })
+                    // Then rerun getItems
+                    props.getItems();
+                }).catch((error) => {
+                    // If axios request failed, alert the user
+                    Swal.fire({
+                        icon: 'warning',
+                        text: 'An error occured while deleting the list, please try again later.'
+                    })
+                    console.log('The Delete failed');
+                })
+            }
+            else if (result.dismiss === Swal.DismissReason.cancel){
+                // If cancelled, alert the user that their list was not reset
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Phew!',
+                    text: 'That was close, your list has NOT been deleted'
+                })
+            }
         })
     }
 
